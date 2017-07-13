eec7031e9e548b49466fc9648978555319c90cac
OTSUKA, Yuanying <yuanying@fraction.jp>
2017-07-13 17:59:57 +0900

    Add test implementation about pod.exec

--- gluster/fs/cmd/glusterfs-simple-provisioner/main.go
+++ gluster/fs/cmd/glusterfs-simple-provisioner/main.go
@@ -18,12 +18,20 @@ package main
 
 import (
 	"flag"
+	"strings"
 
+	"github.com/golang/glog"
 	vol "github.com/kubernetes-incubator/external-storage/gluster/fs/pkg/volume"
+	"github.com/kubernetes-incubator/external-storage/lib/controller"
+	"k8s.io/apimachinery/pkg/util/validation"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 var (
-	provisioner = flag.String("provisioner", "gluster.org/gluster-simple", "Name of the provisioner. The provisioner will only provision volumes for claims that request a StorageClass with a provisioner field set equal to this name.")
+	provisioner = flag.String("provisioner", "gluster.org/glusterfs-simple", "Name of the provisioner. The provisioner will only provision volumes for claims that request a StorageClass with a provisioner field set equal to this name.")
 	master      = flag.String("master", "", "Master URL to build a client config from. Either this or kubeconfig needs to be set if the provisioner is being run out of cluster.")
 	kubeconfig  = flag.String("kubeconfig", "", "Absolute path to the kubeconfig file. Either this or master needs to be set if the provisioner is being run out of cluster.")
 )
@@ -32,6 +40,54 @@ func main() {
 	flag.Set("logtostderr", "true")
 	flag.Parse()
 
-	glusterfsProvisioner := vol.NewGlusterfsProvisioner(nil)
-	print(glusterfsProvisioner)
+	if errs := validateProvisioner(*provisioner, field.NewPath("provisioner")); len(errs) != 0 {
+		glog.Fatalf("Invalid provisioner specified: %v", errs)
+	}
+	glog.Infof("Provisioner %s specified", *provisioner)
+
+	// Create the client according to whether we are running in or out-of-cluster
+	var config *rest.Config
+	var err error
+	if *master != "" || *kubeconfig != "" {
+		glog.Infof("Either master or kubeconfig specified. building kube config from that..")
+		config, err = clientcmd.BuildConfigFromFlags(*master, *kubeconfig)
+	} else {
+		glog.Infof("Building kube configs for running in cluster...")
+		config, err = rest.InClusterConfig()
+	}
+	if err != nil {
+		glog.Fatalf("Failed to create config: %v", err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		glog.Fatalf("Failed to create client: %v", err)
+	}
+
+	// The controller needs to know what the server version is because out-of-tree
+	// provisioners aren't officially supported until 1.5
+	// serverVersion, err := clientset.Discovery().ServerVersion()
+	// if err != nil {
+	// 	glog.Fatalf("Error getting server version: %v", err)
+	// }
+
+	glusterfsProvisioner := vol.NewGlusterfsProvisioner(config, clientset)
+	_, err = glusterfsProvisioner.Provision(controller.VolumeOptions{})
+	if err != nil {
+		glog.Fatalf("Failed to Provision: %v", err)
+	}
+}
+
+// validateProvisioner tests if provisioner is a valid qualified name.
+// https://github.com/kubernetes/kubernetes/blob/release-1.4/pkg/apis/storage/validation/validation.go
+func validateProvisioner(provisioner string, fldPath *field.Path) field.ErrorList {
+	allErrs := field.ErrorList{}
+	if len(provisioner) == 0 {
+		allErrs = append(allErrs, field.Required(fldPath, provisioner))
+	}
+	if len(provisioner) > 0 {
+		for _, msg := range validation.IsQualifiedName(strings.ToLower(provisioner)) {
+			allErrs = append(allErrs, field.Invalid(fldPath, provisioner, msg))
+		}
+	}
+	return allErrs
 }
